23f62ad3faf9234546a15113acdcacd27876a360
objektwerks <objektwerks@runbox.com>
2023-01-06 18:52:23 -0500

    added http server and test

---
+++ src/test/scala/objektwerks/HttpServerTest.scala
@@ -0,0 +1,9 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class HttepServerTest extends AnyFunSuite with Matchers:
+  test("test") {
+    
+  }
