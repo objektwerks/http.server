be767a2c910c607ce17fcdff3acc36f7add7eeb7
objektwerks <objektwerks@runbox.com>
2023-01-07 09:21:55 -0500

    renamed test

--- src/test/scala/objektwerks/ServerTest.scala
+++ src/test/scala/objektwerks/ServerTest.scala
@@ -3,7 +3,7 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class HttepServerTest extends AnyFunSuite with Matchers:
+class ServerTest extends AnyFunSuite with Matchers:
   test("test") {
     
   }
