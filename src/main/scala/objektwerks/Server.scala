153fbf705f5cb603ef5f1418a11c4f519a0f9e37
objektwerks <objektwerks@runbox.com>
2023-01-07 09:33:16 -0500

    added lazy logger to server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -2,8 +2,9 @@ package objektwerks
 
 import com.sun.net.httpserver.HttpServer
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
 
-final class Server:
+final class Server extends LazyLogging:
   val config = ConfigFactory.load("server.conf")
   val host = config.getString("host")
   val port = config.getInt("port")
