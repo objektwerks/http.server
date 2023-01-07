86d96a6b9dc886b4e80e6c59a441ff6969ce479f
objektwerks <objektwerks@runbox.com>
2023-01-07 09:45:07 -0500

    added http server to server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -4,8 +4,13 @@ import com.sun.net.httpserver.HttpServer
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
-final class Server extends LazyLogging:
+import java.net.InetSocketAddress
+
+object Server extends LazyLogging:
   val config = ConfigFactory.load("server.conf")
   val host = config.getString("host")
   val port = config.getInt("port")
-  
+  val backlog = 0
+  val http = HttpServer.create(new InetSocketAddress(port), backlog)
+
+  @main def main(): Unit = logger.info(s"Http Server started at: $host:$port")
