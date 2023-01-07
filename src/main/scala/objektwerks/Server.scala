0e8e2420f0f848b3afa9c2e738bcc0514cbfb62c
objektwerks <objektwerks@runbox.com>
2023-01-07 09:31:09 -0500

    added config to server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -1,6 +1,10 @@
 package objektwerks
 
 import com.sun.net.httpserver.HttpServer
+import com.typesafe.config.ConfigFactory
 
 final class Server:
+  val config = ConfigFactory.load("server.conf")
+  val host = config.getString("host")
+  val port = config.getInt("port")
   
