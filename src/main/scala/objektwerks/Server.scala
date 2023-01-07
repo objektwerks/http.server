537b3ea9dbbeecb916f36d9988065f79839ede00
objektwerks <objektwerks@runbox.com>
2023-01-07 10:04:35 -0500

    added handler to server

--- src/main/scala/objektwerks/Server.scala
+++ src/main/scala/objektwerks/Server.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import com.sun.net.httpserver.HttpServer
+import com.sun.net.httpserver.{HttpExchange, HttpHandler, HttpServer}
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
@@ -12,10 +12,16 @@ object Server extends LazyLogging:
   val host = config.getString("host")
   val port = config.getInt("port")
   val backlog = config.getInt("backlog")
+
   val http = HttpServer.create(InetSocketAddress(port), backlog)
+  val handler = new HttpHandler {
+    override def handle(exchange: HttpExchange): Unit = ???
+  }
 
   @main def main(): Unit =
     http.setExecutor(Executors.newVirtualThreadPerTaskExecutor())
+    http.createContext("/", handler)
+
     logger.info(s"*** Http Server started at: $host:$port")
 
   sys.addShutdownHook {
