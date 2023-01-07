1a7fe7cac010fa9b9ed56fb9d2c234de6a501cea
objektwerks <objektwerks@runbox.com>
2023-01-07 09:36:02 -0500

    edited server test

--- src/test/scala/objektwerks/ServerTest.scala
+++ src/test/scala/objektwerks/ServerTest.scala
@@ -4,6 +4,6 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class ServerTest extends AnyFunSuite with Matchers:
-  test("test") {
+  test("server") {
     
   }
