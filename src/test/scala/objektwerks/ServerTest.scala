76d6d9fdca342f1d1195553b98ddeca218db5361
objektwerks <objektwerks@runbox.com>
2023-01-08 13:45:13 -0500

    removed test dir

--- src/test/scala/objektwerks/ServerTest.scala
+++
@@ -1,9 +0,0 @@
-package objektwerks
-
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-
-class ServerTest extends AnyFunSuite with Matchers:
-  test("server") {
-    
-  }
