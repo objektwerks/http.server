25aec5bedadf1f858251791b4e46e27f084dad83
objektwerks <objektwerks@runbox.com>
2023-01-14 13:27:25 -0500

    added client

--- README.md
+++ README.md
@@ -9,10 +9,12 @@ JDK 19
 
 Server
 ------
+>See Server.
 1. sbt clean compile run
 
 Client
 ------
+>See Client.
 1. curl -v http://localhost:7979/now
 
 Reference
---
+++ src/main/scala/objektwerks/Client.scala
@@ -0,0 +1,28 @@
+package objektwerks
+
+import com.typesafe.scalalogging.LazyLogging
+
+import java.net.URI
+import java.net.http.{HttpClient, HttpRequest}
+import java.net.http.HttpResponse.BodyHandlers
+import java.time.Duration
+import java.time.temporal.ChronoUnit.SECONDS
+
+object Client extends LazyLogging:
+  def call(url: String,
+           requestJson: String): String =
+    logger.info(s"url: $url")
+    logger.info(s"request json: $requestJson")
+    val request = HttpRequest
+      .newBuilder
+      .uri(URI(url))
+      .timeout(Duration.of(10, SECONDS))
+      .version(HttpClient.Version.HTTP_2)
+      .headers("Content-Type", "application/json; charset=UTF-8", "Accept", "application/json")
+      .POST( HttpRequest.BodyPublishers.ofString(requestJson) )
+      .build
+    
+    val client = HttpClient.newHttpClient
+    val responseJson = client.send( request, BodyHandlers.ofString ).body
+    logger.info(s"response json: $requestJson")
+    responseJson
