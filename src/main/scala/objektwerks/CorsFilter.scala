47c950f141860d75e3b5905ffad80be0316e9e4d
objektwerks <objektwerks@runbox.com>
2023-02-13 12:55:56 -0500

    edited cors filter

---
+++ src/main/scala/objektwerks/CorsFilter.scala
@@ -0,0 +1,17 @@
+package objektwerks
+
+import com.sun.net.httpserver.Filter
+import com.sun.net.httpserver.Filter.Chain
+import com.sun.net.httpserver.HttpExchange
+
+class CorsFilter extends Filter:
+  override def doFilter(exchange: HttpExchange, chain: Chain): Unit =
+    val headers = exchange.getResponseHeaders()
+    headers.add("Content-Type", "application/json; charset=UTF-8")
+    headers.add("Access-Control-Allow-Origin", "*")
+    headers.add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+    headers.add("Access-Control-Allow-Headers", "*")
+    headers.add("Access-Control-Allow-Credentials", "true")
+    headers.add("Access-Control-Allow-Credentials-Header", "*")
+
+  override def description(): String = "Cors Filter"
