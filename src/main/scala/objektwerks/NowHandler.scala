7ee3f426aa32fc8b8e19692263bc49f70069af53
objektwerks <objektwerks@runbox.com>
2023-02-13 14:29:46 -0500

    edited now handler

--- src/main/scala/objektwerks/NowHandler.scala
+++ src/main/scala/objektwerks/NowHandler.scala
@@ -8,6 +8,7 @@ class NowHandler extends HttpHandler:
   override def handle(exchange: HttpExchange): Unit =
     val response = Instant.now.toString
     exchange.sendResponseHeaders(200, response.length())
+    exchange.getResponseHeaders().add("Content-Type", "application/json; charset=UTF-8")
     val outputStream = exchange.getResponseBody
     outputStream.write(response.getBytes())
     outputStream.flush()
